0616630ca21d9bfdfb8c6371cc2a5f985767e029
Daniel Penelva <d4n.andrade@gmail.com>
2023-03-16 02:31:05 -0300

    Implementando MainActivity.xml

--- app/src/main/java/com/formacaoandroidstudio/sorteio/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/sorteio/MainActivity.java
@@ -3,6 +3,10 @@ package com.formacaoandroidstudio.sorteio;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void gerarSorteio(View view){
+
+        TextView numSorteio = findViewById(R.id.textoResultado);
+
+        int numAleatorio = new Random().nextInt(11);
+
+        numSorteio.setText("Número: " + numAleatorio);
     }
 }
